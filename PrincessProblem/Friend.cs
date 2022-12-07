aea96519a5f58db777e37df3e39194363ece6790
Alexandr Baranov <baranovau216@gmail.com>
2022-12-07 19:50:44 +0700

    throw exception if unfamiliar contender & refactor

--- PrincessProblem/Friend.cs
+++ PrincessProblem/Friend.cs
@@ -1,21 +1,33 @@
+using PrincessProblem.Exceptions;
+
 namespace PrincessProblem;
 
 public class Friend
 {
-    private readonly Dictionary<string, int> _contenders;
-    
+    private readonly Dictionary<string, int> _contenders = new();
+    private readonly HashSet<string> _familiarContendersNames = new();
+
     public Friend(List<Contender> contenders)
     {
-        _contenders = new Dictionary<string, int>();
-
         foreach (var contender in contenders)
         {
             _contenders.Add(contender.Name, contender.Rank);
         }
     }
 
+    public void MarkAsFamiliar(string contenderName)
+    {
+        _familiarContendersNames.Add(contenderName);
+    }
+
     public string ChooseBest(string firstContenderName, string secondContenderName)
     {
+        if (!_familiarContendersNames.Contains(firstContenderName) ||
+            !_familiarContendersNames.Contains(secondContenderName))
+        {
+            throw new UnfamiliarContender();
+        }
+        
         if (_contenders[firstContenderName] < _contenders[secondContenderName])
         {
             return firstContenderName;
